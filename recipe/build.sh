2c0e3817491eb8d7cac522c17884fa9b2e716ebe
Filipe Fernandes <ocefpaf@gmail.com>
2017-08-02 10:47:05 -0300

    no x11 on OS X

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,13 @@ export CPPFLAGS="-I${PREFIX}/include $CPPFLAGS"
 export CFLAGS="-I${PREFIX}/include $CFLAGS"
 
 if [ "$(uname)" = "Darwin" ]; then
+    if [ -d "/opt/X11" ]; then
+        x11_lib="-L/opt/X11/lib"
+        x11_inc="-I/opt/X11/include -I/opt/X11/include/freetype2"
+    else
+        echo "No X11 libs found. Exiting..." 1>&2
+        exit
+    fi
     LDFLAGS="-headerpad_max_install_names $LDFLAGS"
     conf_file=config/Darwin_Intel
 elif [ "$(uname)" = "Linux" ]; then
