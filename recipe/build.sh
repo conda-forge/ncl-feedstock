c4a28fdadc852e907b944c4c326a4fc51a5e520d
Kevin Hallock <hallock@ucar.edu>
2017-11-02 18:03:37 -0600

    Force use of XQuartz-provided cairo on Mac
    Cairo is currently being included as a dependency of poppler, which is
    itself a dependency of gdal. This makes building NCL on Mac difficult,
    as the conda-installed cairo was taking precedence over the XQuartz
    version.

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,6 +10,9 @@ if [ "$(uname)" = "Darwin" ]; then
     if [ -d "/opt/X11" ]; then
         x11_lib="-L/opt/X11/lib"
         x11_inc="-I/opt/X11/include -I/opt/X11/include/freetype2"
+
+        CAIROLIB="#define CAIROlib /opt/X11/lib/libcairo.2.dylib /opt/X11/lib/libfontconfig.1.dylib /opt/X11/lib/libpixman-1.0.dylib /opt/X11/lib/libfreetype.6.dylib -lXrender -lexpat -lpng -lz -liconv -lbz2 -lpthread"
+        CAIROLIBUSER="#define CAIROlibuser /opt/X11/lib/libcairo.2.dylib /opt/X11/lib/libfontconfig.1.dylib /opt/X11/lib/libpixman-1.0.dylib /opt/X11/lib/libfreetype.6.dylib -lXrender -lexpat -lpng -lz -liconv -lbz2 -lpthread"
     else
         echo "No X11 libs found. Exiting..." 1>&2
         exit
@@ -31,7 +34,7 @@ sed -e "s|^\(  set cpp = \)/lib/cpp$|\1${PREFIX}/bin/cpp|g" -i.backup config/yma
 # fix path to cpp in $conf_file
 sed -e "s|/usr/bin/cpp|${PREFIX}/bin/cpp|g" -i.backup ${conf_file}
 
-sed -e "s|\${PREFIX}|${PREFIX}|g" -e "s|\${x11_inc}|${x11_inc}|g" -e "s|\${x11_lib}|${x11_lib}|g" "${RECIPE_DIR}/Site.local.template" > config/Site.local
+sed -e "s|\${PREFIX}|${PREFIX}|g" -e "s|\${x11_inc}|${x11_inc}|g" -e "s|\${x11_lib}|${x11_lib}|g" -e "s|\${CAIROLIB}|${CAIROLIB}|g" -e "s|\${CAIROLIBUSER}|${CAIROLIBUSER}|g" "${RECIPE_DIR}/Site.local.template" > config/Site.local
 
 echo -e "n\n" | ./Configure
 make Everything
