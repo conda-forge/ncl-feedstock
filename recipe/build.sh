814e0351cb5bfd745c0d41d552e52cf1b827bf88
Isuru Fernando <isuruf@gmail.com>
2021-01-01 12:47:31 -0600

    cleanup

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
 export CXXFLAGS="-fPIC ${CXXFLAGS}"
-export LDFLAGS="-L${PREFIX}/lib ${LDFLAGS}"
-export CPPFLAGS="-I${PREFIX}/include -DACCEPT_USE_OF_DEPRECATED_PROJ_API_H=1 ${CPPFLAGS}"
-export CFLAGS="-I${PREFIX}/include -DACCEPT_USE_OF_DEPRECATED_PROJ_API_H=1 ${CFLAGS}"
+export CPPFLAGS="-DACCEPT_USE_OF_DEPRECATED_PROJ_API_H=1 ${CPPFLAGS}"
+export CFLAGS="-DACCEPT_USE_OF_DEPRECATED_PROJ_API_H=1 ${CFLAGS}"
 
 if [ "$(uname)" = "Darwin" ]; then
     export CC="${CLANG}"
     export CPP="clang-cpp -traditional"
-    export CXX="${CLANG}++"
+    export CXX="${CLANGXX}"
     export FC
 
     export PATH="${PATH}:/opt/X11/bin"
