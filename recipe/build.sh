a54efdae7df7036aa95fc5b617a19d2e8d1850b0
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2022-03-14 18:02:42 +0100

    Use the narrower `-fallow-argument-mismatch` instead of full on std=legacy

--- recipe/build.sh
+++ recipe/build.sh
@@ -35,7 +35,7 @@ elif [ "$(uname)" = "Linux" ]; then
     export CPP="${CPP} -traditional"
     # export CXX="${GXX}"
     # export FC="${FC} -std=legacy"
-    export EXTRA_FCOPTIONS="-std=legacy"
+    export EXTRA_FCOPTIONS="-fallow-argument-mismatch"
 
     conf_file=config/LINUX
 fi
